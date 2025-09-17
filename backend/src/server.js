2dced60d8524ee69fde10fbb1207db5894574b30
MostakemHossain <mostakememon123@gmail.com>
2025-09-17 21:45:23 +0600

    deploying app in servella

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from "path";
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from "./routes/message.route.js";
@@ -6,10 +7,23 @@ import messageRoutes from "./routes/message.route.js";
 const app= express();
 dotenv.config();
 const PORT= process.env.PORT || 4500;
+const __dirname= path.resolve(); 
+
 
 
 app.use("/api/auth",authRoutes);
 app.use("/api/message",messageRoutes);
+
+// make ready for deployment
+if(process.env.NODE_ENV==="production"){
+ 
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get((req, res, next) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    });
+}
+
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
